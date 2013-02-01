c50bd0dc30ed259fc2d7228f9f2936244f572a71
Hernan Fernandez <hernanjx@gmail.com>
2013-02-01 12:03:21 -0300

    init repo

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+gem 'sqlite3'
+gem 'thin'
+
+gem 'redis'
+gem 'recommendable'
+gem 'resque', :require => "resque/server"
+gem 'resque-loner'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tita::Application.config.secret_token = '15da6f6d6da38d6f6a9d357c2fe78a63b4a6540587dfd20b01f01a7ab7797128ea2d55715b94ed0f75fc77e56d0be047899433afadfa918332f6b8be6b65a8db'
